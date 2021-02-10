eb03aebaf206e8f5ca2d0c2bc93929d0c45ddad0
jesus dario rodriguez <44887174+JDRodriguez80@users.noreply.github.com>
2021-02-10 04:01:58 -0600

    Update Pila_A2_JER.c

--- Pila_A2_JER.c
+++ Pila_A2_JER.c
@@ -4,8 +4,6 @@
 #include<stdbool.h>
 
 #define ARREGLO_MAX 100
-
-int main() {
 	int dpila[ARREGLO_MAX];
 	int eaux1;
 	int eopc;
@@ -14,6 +12,15 @@ int main() {
 	int i;
 	bool lmargen;
 	int n;
+	n = 0;
+	i = 0;
+	eopc = 0;
+	etope = 0;
+	eaux1 = 1;
+	evalor = 0;
+
+int main() {
+
 	/* Nombre del alumno: Jesus Dario Rodriguez */
 	/* Matricula:ES1822021661 */
 	/* Universidad Abierta y a Distancia de México UnADM  */
@@ -21,13 +28,7 @@ int main() {
 	/* Carrera:Ingenieria en Desarrollo de Software */
 	/* Materia:Estructura de Datos  */
 	/* Docente:Ing. Alfredo Orozco Escobar */
-	n = 0;
-	i = 0;
-	eopc = 0;
-	etope = 0;
-	eaux1 = 1;
-	evalor = 0;
-	lmargen = false;
+
 	printf("Este programa demuestra las operaciones en la estructura de datos PILA o STACK\n");
 	printf("De cuantos elementos quiere la pila?\n");
 	scanf("%i",&n);
@@ -45,25 +46,12 @@ int main() {
 		scanf("%i",&eopc);
 		switch (eopc) {
 		case 1:
-			if ((lmargen==true)) {
-				printf("Overflow error-- Pila llena\n");
-			} else {
-				printf("Introduzca un numero entero\n");
-				scanf("%i",&evalor);
-				printf("Se introdujo el valor:%i a la pila\n",evalor);
-				if (etope==n) {
-					printf("Se alcanzo el cupo maximo permitido en la pila\n");
-				} else {
-					etope = etope+1;
-					dpila[etope-1] = evalor;
-					if (etope==n) {
-						printf("Se alcanzo el cupo maximo permitido en la pila\n");
-					}
-				}
-			}
-			/* verificar que la pila este llena */
+		
+		
+			push();
 			break;
 		case 2:
+			
 			if ((etope==n)) {
 				lmargen = true;
 				printf("la pila esta llena\n");
@@ -71,9 +59,10 @@ int main() {
 				lmargen = false;
 				printf("La pila aun tiene espacios\n");
 			}
-			/* comprobar que la pila esta vacia */
+			
 			break;
 		case 3:
+		
 			if ((etope==0)) {
 				lmargen = true;
 				printf("la pila esta vacia\n");
@@ -81,17 +70,11 @@ int main() {
 				lmargen = false;
 				printf("la pila no esta vacia\n");
 			}
-			/* eliminando elemento */
+			
 			break;
 		case 4:
-			if ((etope==0)) {
-				printf("Error: derflow: pila vacia\n");
-			} else {
-				evalor = dpila[etope-1];
-				etope = etope-1;
-				printf("se elimino un lemento de manera correcta\n");
-			}
-			/* mostrando en pantalla la situacuion de la pila actual */
+		
+			pop();
 			break;
 		case 5:
 			printf("elemntos ingresados: %i\n",etope);
@@ -107,3 +90,31 @@ int main() {
 	return 0;
 }
 
+void push(){
+		if ((lmargen==true)) {
+				printf("Overflow error-- Pila llena\n");
+			} else {
+				printf("Introduzca un numero entero\n");
+				scanf("%i",&evalor);
+				printf("Se introdujo el valor:%i a la pila\n",evalor);
+				if (etope==n) {
+					printf("Se alcanzo el cupo maximo permitido en la pila\n");
+				} else {
+					etope = etope+1;
+					dpila[etope-1] = evalor;
+					if (etope==n) {
+						printf("Se alcanzo el cupo maximo permitido en la pila\n");
+					}
+				}
+			}
+
+}
+void pop(){
+		if ((etope==0)) {
+				printf("Error: derflow: pila vacia\n");
+			} else {
+				evalor = dpila[etope-1];
+				etope = etope-1;
+				printf("se elimino un lemento de manera correcta\n");
+			}	
+}
