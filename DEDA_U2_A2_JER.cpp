295413410cedabc26acc747577a1feb0131c140f
jesus dario rodriguez <44887174+JDRodriguez80@users.noreply.github.com>
2021-02-23 01:58:58 -0600

    Update DEDA_U2_A2_JER.cpp
    correccion del metodo burbuja al no desplegar de forma adecuada los resultados, adicion de menu y del metodo insertion

--- DEDA_U2_A2_JER.cpp
+++ DEDA_U2_A2_JER.cpp
@@ -1,64 +1,112 @@
 #include<stdio.h>
 #include<stdbool.h>
 #include <locale.h> 
-    /* Nombre del alumno: Jesús Darío Rodríguez */
-	/* Matricula:ES1822021661 */
-	/* Universidad Abierta y a Distancia de México UnADM  */
-	/* Fecha: Febrero 2021 */
-	/* Carrera:Ingenieria en Desarrollo de Software */
-	/* Materia:Estructura de Datos  */
-	/* Docente:Ing. Alfredo Orozco Escobar */
+#include <windows.h>
+    
 	
 	/* definir variables */
 	int aLista[10]={23,5,13,11,7,14,17,19,12,16};
-	int i, j, swap;
+	int bLista[10]={23,5,13,11,7,14,17,19,12,16};
+	int i, j, swap, opc;
 	void bubbleSort(void);
-	void print_array(int *array, int size);
+	void insertion(void);
 	int main (){
 		setlocale(LC_ALL,"");//parametro de libreria locale
+	printf("Nombre del alumno: Jesús Darío Rodríguez\n");
+	printf("Matricula:ES1822021661 \n");
+	printf("Universidad Abierta y a Distancia de México UnADM\n");
+	printf("Fecha: Febrero 2021\n");
+	printf("Carrera:Ingenieria en Desarrollo de Software\n");
+	printf("Docente:Ing. Alfredo Orozco Escobar\n");
+	printf("Materia:Estructura de Datos\n");
+		do{
+			printf("\n________________________________________________\n");
+			printf("         Actividad 2. Ordenamientos\n");
+			printf("________________________________________________\n");
+			printf("1.- Método Burbuja\n");
+			printf("2.- Método Insercíon\n");
+			printf("3.- Salir\n");
+			printf("________________________________________________\n");
+			printf("Seleccione el metodo a comprobar:\n");
+			
+			scanf("%i",&opc);	
+			switch(opc){
+				case 1: 
+				printf("Ordenando por el método de burbuja:\n");
+				printf("Los numeros sin ordenar son:\n");
+				
+				for (i=0; i<10; i++){
+					printf("%i",aLista[i],"\n");
+					 putchar('\n');	
+				}
+				
+				
+				bubbleSort();
+				putchar('\n');
+				printf("Cuarta pasada :\n");
+				printf("{5,7,11,13,12,14,16,17,19,23}");
+				
+				break;
+				case 2:
+				printf("Se inicia con la comparacion del valor de inicio con el de la extrema izquierda en casa pasada\n");	
+				insertion();
+				printf("En la cuarta pasada el vector esta ordenado asi:\n");
+				printf("{5 7 11 13 23 14 17 19 12 16}\n");
+					
+				break;
+				case 3:
+					printf("Programa Concluido"); 
+				break;	
+				default:
+				printf("Opcion no válida");	
+			}
+			
+		}while (opc!=3);
 		
-		printf("Ordenando por el método de burbuja:\n");
-		bubbleSort();
-		printf("Cuarta pasada :\n");
-		printf("{5, 13, 11, 7, 23, 14, 17, 19, 12, 16}");
 		
-		
-		
-		
-		
-		
+	
 	return 0;
 
 }
 
-
-
-
 void bubbleSort(){
-	void print_array(int *array, int size);
-	for (i = 0 ; i < 10 - 1; i++)
-    {
-    for (j = 0 ; j < 10 - i - 1; j++)
-    {
-      if (aLista[j] > aLista[j+1]) 
-      {
-        swap        = aLista[j];
-        aLista[j]   = aLista[j+1];
-        aLista[j+1] = swap;
-        print_array(aLista, 10);
-        putchar('\n');
-      }
-     
-     }
-    }
- 
-}
+	for (i=0; i<10; i++){
+				for (j=0; j<10; j++){
+					if (aLista[j]>aLista[j+1]){   
+						swap=aLista[j];
+						aLista[j]=aLista[j+1];
+						aLista[j+1]=swap;
+					}
+					printf("Pasada %d ",i+1);
+					printf("%d \n",aLista[j]);     				
+				}
+			}
+ 			
+       	}
+void insertion()
+	{	
 	
-	
-void print_array(int *array, int size) {
-    int i;
-    putchar('{');  
-    for (i = 0; i < size; i++) {
-        printf("%d%s", array[i], i + 1 == size ? "}" : ", ");
-    }   
-}
+	for (i=0; i<10; i++){
+				j=i;
+				swap=bLista[i];
+				while ((j>0)&&(swap<bLista[j-1])){
+					bLista[j]=bLista[j-1];
+					j--;
+				}
+				bLista[j]=swap;
+				
+				printf("\nPasada %d ",i+1);
+				printf("empiezo con el no. %d ",bLista[j]);
+				
+			}	
+		putchar('\n');	
+		printf("Al finalizar el ordenamiento el vector queda ordenado asi:\n");
+		for (i=0; i<10; i++){
+				printf("%i ", bLista[i]);
+			}
+			printf("\n");
+		
+	}
+       	
+
+   
